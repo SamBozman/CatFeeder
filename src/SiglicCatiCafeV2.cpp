40a96bded45506f846011876cc7c8a87cdc0a3b6
SamBozman <s.j.bozman@gmail.com>
2023-08-29 07:35:43 -0700

    Added in stepper motor control code

--- src/SiglicCatiCafeV2.cpp
+++ src/SiglicCatiCafeV2.cpp
@@ -1,32 +1,26 @@
-/*
-   -------------------------------------------------------------------------------------
-   HX711_ADC
-   Arduino library for HX711 24-Bit Analog-to-Digital Converter for Weight
-   Scales Olav Kallhovd sept2017
-   -------------------------------------------------------------------------------------
-*/
 
-/*
-   Settling time (number of samples) and data filtering can be adjusted in the
-   config.h file For calibration and storing the calibration value in eeprom,
-   see example file "Calibration.ino"
-
-   The update() function checks for new data and starts the next conversion. In
-   order to acheive maximum effective sample rate, update() should be called at
-   least as often as the HX711 sample rate; >10Hz@10SPS, >80Hz@80SPS. If you
-   have other time consuming code running (i.e. a graphical LCD), consider
-   calling update() from an interrupt routine, see example file
-   "Read_1x_load_cell_interrupt_driven.ino".
-
-   This is an example sketch on how to use this library
-*/
 
 #include <HX711_ADC.h>
 #if defined(ESP8266) || defined(ESP32) || defined(AVR)
 #include <EEPROM.h>
 #endif
+#include <AccelStepper.h>
 
-// pins:
+// Motor pin definitions:
+#define motorPin1 8  // IN1 on the ULN2003 driver
+#define motorPin2 9  // IN2 on the ULN2003 driver
+#define motorPin3 10 // IN3 on the ULN2003 driver
+#define motorPin4 11 // IN4 on the ULN2003 driver
+
+// Define the AccelStepper interface type; 4 wire motor in half step mode:
+#define MotorInterfaceType 8
+
+// Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper
+// library with 28BYJ-48 stepper motor:
+AccelStepper stepper = AccelStepper(MotorInterfaceType, motorPin1, motorPin3,
+                                    motorPin2, motorPin4);
+
+// HX711 pins:
 const int HX711_dout = 4; // mcu > HX711 dout pin
 const int HX711_sck = 5;  // mcu > HX711 sck pin
 
@@ -36,6 +30,11 @@ HX711_ADC LoadCell(HX711_dout, HX711_sck);
 const int calVal_eepromAdress = 0;
 unsigned long t = 0;
 
+// static float previousCellVal = 0;
+const float KonaWeight = 5045; // Kona Cat weight as of Aug, 2023
+// TODO: const float LuckyWeight = ??
+
+//*****************************************************************************************************
 void setup() {
   Serial.begin(57600);
   delay(10);
@@ -56,14 +55,17 @@ void setup() {
 
   //!&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
   EEPROM.get(calVal_eepromAdress,
-             calibrationValue); // uncomment this if you want to fetch the
-                                // calibration value from eeprom
+             calibrationValue); //! uncomment this if you want to fetch the
+                                //! calibration value from eeprom
 
   unsigned long stabilizingtime =
       2000; // preciscion right after power-up can be improved by adding a few
             // seconds of stabilizing time
-  boolean _tare = true; // set this to false if you don't want tare to be
-                        // performed in the next step
+
+  //!&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+  boolean _tare = true; //! set this to false if you don't want tare to be
+                        //! performed in the next step (Load and set 0 weight)
+
   LoadCell.start(stabilizingtime, _tare);
   if (LoadCell.getTareTimeoutFlag()) {
     Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
@@ -75,6 +77,7 @@ void setup() {
   }
 }
 
+//*****************************************************************************************************
 void loop() {
   static boolean newDataReady = 0;
   const int serialPrintInterval =
